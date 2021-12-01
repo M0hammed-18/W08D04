31dbdb6aef5cf84a86e165b6897155634562045f
M0hammed-18 <Mohammed.Alweheid.elq.js@tuwaiq.edu.sa>
2021-12-01 13:53:09 +0300

    AutorizationFileDone

--- routers/middleware/authorization.js
+++ routers/middleware/authorization.js
@@ -1,1 +1,21 @@
+//Create This File To Allow Who Can Edit Information
+const roleModel = require("./../../db/models/role");
 
+const authorization = async (req, res, next) => {
+  try {
+    console.log(req);
+    const roleId = req.token.role;
+    console.log(roleId);
+    const result = await roleModel.findById(roleId);
+    console.log(result);
+    if (result.role === "admin") {
+      next();
+    } else {
+      return res.status(403).json({ massage: "forbidden" });
+    }
+  } catch (error) {
+    res.status(403).json(error);
+  }
+};
+
+module.exports = authorization;
