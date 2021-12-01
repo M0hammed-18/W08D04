c7a922c8070faec42b869bc4289950c49a121eae
M0hammed-18 <Mohammed.Alweheid.elq.js@tuwaiq.edu.sa>
2021-12-01 12:54:55 +0300

    UserSchemaDone

---
+++ db/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const user =new mongoose.Schema({
+    email:{type:String,required:true,unique:true},
+    username:{type:String,required:true,unique:true},
+    password:{type:String,required:true},
+    role:{type: mongoose.Schema.Types.ObjectId, ref: "Role"},
+    avter:{type:String},
+    delet:{type:Boolean}
+})
+
+module.exports=mongoose.model("User",user);
